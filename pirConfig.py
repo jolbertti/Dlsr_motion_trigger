8fef6b42e36d01b94940b771f4c52f8b32495f67
Joel Hiltunen <84034458+jolbertti@users.noreply.github.com>
2022-05-02 00:14:34 +0300

    Update pirConfig.py

--- pirConfig.py
+++ pirConfig.py
@@ -2,19 +2,20 @@ from machine import Pin
 import time
 
 #delay between shutter release signals sent to camera
-#REPLACE WITH INPUT FROM SWITCH
+#TODO REPLACE WITH INPUT FROM SWITCH
 shotdelay = 4
 
 #how many shutter release signals are sent to camera
-#REPLACE WITH INPUT FROM SWITCH
+#TODO REPLACE WITH INPUT FROM SWITCH
 photos = 5
 
 #delay from initial pir signal to shutter release signal sent to camera 
-#REPLACE WITH INPUT FROM SWITCH
+#TODO REPLACE WITH INPUT FROM SWITCH
 wakeup = 2
 
 
 pir = Pin(22, Pin.IN, Pin.PULL_DOWN)
+cameraSignal = Pin(21, Pin.OUT, Pin.PULL_DOWN)
  
 #print('Starting up the PIR Module')
 #time.sleep(1)
@@ -24,5 +25,7 @@ while True:
     if pir.value() == 1:
         time.sleep(wakeup)
         for i in range(photos):
-            print('Click nro' ,i)
+            cameraSignal.high()
+            time.sleep(0.5)
+            cameraSignal.low()
             time.sleep(shotdelay)
